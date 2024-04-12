593297838dfaceb576a750f5a418d645854c7e23
James Ritzman <jamesritzman@yahoo.com>
2024-04-11 20:19:14 -0700

    initial layout with mock inputs

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
+import Form from './pages/Form'
 import './App.css'
 
 function App() {
 
-  return (
-    <>
-    </>
-  )
+    return (
+        <>
+            {/* In a full-fledged application, this would contain Routes to Pages. */}
+            {/* Due to the simple nature of this project, I'll forgo adding unneeded routing complexity. */}
+            <Form />
+        </>
+    )
 }
 
 export default App
