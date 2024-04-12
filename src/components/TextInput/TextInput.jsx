96db80cb8258264cc38a9fe6784ef4cde0206312
James Ritzman <jamesritzman@yahoo.com>
2024-04-11 21:26:43 -0700

    additional functionality for text input, inc. validation via mock-api

--- src/components/TextInput/TextInput.jsx
+++ src/components/TextInput/TextInput.jsx
@@ -1,10 +1,27 @@
+import { isNameValid } from '../../mock-api/apis'
 import './TextInput.css'
+
+
+async function handleInputChange(e, getsetValue, getsetValidation) {
+    // set the new value of the input based on the newly typed text
+    getsetValue.setName(e.target.value);
+
+    // commpare the new value of the input to list of valid names per "api"
+    // Note: can NOT use getsetValue.name because .setName is async (.get may not reflect the new value yet)
+
+    // Trim leading/trailing white space from the input so that a user can't simply add spaces before/after the name
+    //  in order to create a duplicate-looking value
+    const isValid = await isNameValid(e.target.value.trim());
+    getsetValidation.setNameIsValid(isValid);
+}
+
 
 function TextInput(props) {
     const {
         labelText = 'Input:',
         inputId,
         getsetValue,
+        getsetValidation,
         placeholderText = 'Enter text here...',
         containerClassname = ''
     } = props;
@@ -13,8 +30,14 @@ function TextInput(props) {
         <div className={`textinput-component ${containerClassname}`}>
             <label htmlFor={inputId}>{labelText}</label>
             <div className="input-wrapper">
-                <input type="text" placeholder={placeholderText} id={inputId} value={getsetValue.get} onChange={getsetValue.set} />
-                <p className="validation-message">this name has already been taken</p>
+                <input
+                    type="text"
+                    placeholder={placeholderText}
+                    id={inputId}
+                    value={getsetValue.name}
+                    onChange={(e) => handleInputChange(e, getsetValue, getsetValidation)}
+                />
+                { !getsetValidation.nameIsValid && <p className="validation-message">this name has already been taken</p> }
             </div>
         </div>
     )
--- src/pages/Form.jsx
+++ src/pages/Form.jsx
@@ -5,7 +5,8 @@ import TextInput from '../components/TextInput/TextInput'
 import './Form.css'
 
 function Form(props) {
-    const [name, setName] = useState("");
+    const [name, setName] = useState(""); // value of the Name input
+    const [nameIsValid, setNameIsValid] = useState(true); // is the value of the Name input valid (unique)
 
     return (
         <main className="form-page">
@@ -14,6 +15,7 @@ function Form(props) {
                     labelText="Name"
                     inutId="name-input"
                     getsetValue={{name: name, setName: setName}}
+                    getsetValidation={{nameIsValid: nameIsValid, setNameIsValid: setNameIsValid}}
                     placeholderText="Please enter a unique name"
                     containerClassname="grid-container"
                 />
